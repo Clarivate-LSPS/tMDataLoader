2967fdbc61cf5504a7a5988d9bce463975e4667a
bondarev <alexander.i.bondarev@gmail.com>
2014-03-07 16:53:35 +0400

    Add Postgres optimizations

---
+++ sql-postgres/optimizations/median.sql
@@ -0,0 +1,46 @@
+-- borrowed from: http://wiki.postgresql.org/wiki/Aggregate_Median
+-- insert as "tm_cz"?
+
+CREATE OR REPLACE FUNCTION tm_cz._final_median(anyarray)
+   RETURNS numeric AS
+$$
+   SELECT AVG(val)
+   FROM (
+     SELECT val
+     FROM unnest($1) val
+     ORDER BY 1
+     LIMIT  2 - MOD(array_upper($1, 1), 2)
+     OFFSET CEIL(array_upper($1, 1) / 2.0) - 1
+   ) sub;
+$$
+LANGUAGE 'sql' IMMUTABLE;
+
+DROP AGGREGATE IF EXISTS tm_cz.median(anyelement);
+CREATE AGGREGATE tm_cz.median(anyelement) (
+  SFUNC=array_append,
+  STYPE=anyarray,
+  FINALFUNC=tm_cz._final_median,
+  INITCOND='{}'
+);
+
+CREATE OR REPLACE FUNCTION tm_cz._final_median(double precision[])
+  RETURNS double precision AS
+  $$
+   SELECT AVG(val)
+   FROM (
+     SELECT val
+     FROM unnest($1) val
+     ORDER BY 1
+     LIMIT  2 - MOD(array_upper($1, 1), 2)
+     OFFSET CEIL(array_upper($1, 1) / 2.0) - 1
+   ) sub;
+$$
+LANGUAGE 'sql' IMMUTABLE;
+
+DROP AGGREGATE IF EXISTS tm_cz.median(double precision);
+CREATE AGGREGATE tm_cz.median(double precision) (
+SFUNC=array_append,
+STYPE=double precision[],
+FINALFUNC=tm_cz._final_median,
+INITCOND='{}'
+);
