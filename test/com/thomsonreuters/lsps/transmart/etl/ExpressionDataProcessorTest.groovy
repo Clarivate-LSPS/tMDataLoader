59453a09296f5e9fe04202f8a6668fef12f5f517
bondarev <alexander.i.bondarev@gmail.com>
2014-03-24 17:25:37 +0400

    Add test for samples merge when expression data loaded

--- test/com/thomsonreuters/lsps/transmart/etl/ExpressionDataProcessorTest.groovy
+++ test/com/thomsonreuters/lsps/transmart/etl/ExpressionDataProcessorTest.groovy
@@ -1,22 +1,64 @@
 package com.thomsonreuters.lsps.transmart.etl
+
+import groovy.sql.Sql
+
+import static org.hamcrest.CoreMatchers.notNullValue
+import static org.junit.Assert.assertThat
+
 /**
  * Created by bondarev on 2/24/14.
  */
 class ExpressionDataProcessorTest extends GroovyTestCase {
-    void testProcessFiles() {
-        def processor = new ExpressionDataProcessor([
-                logger: new Logger([isInteractiveMode: true]),
-                db: [
-                        jdbcConnectionString: 'jdbc:postgresql:transmart',
-                        username: 'postgres',
-                        password: 'postgres',
-                        jdbcDriver: 'org.postgresql.Driver'
-                ],
-                controlSchema: 'tm_cz',
+    def connectionSettings = [
+            jdbcConnectionString: 'jdbc:postgresql:transmart',
+            username            : 'postgres',
+            password            : 'postgres',
+            jdbcDriver          : 'org.postgresql.Driver'
+    ]
+
+    private Sql _sql
+    private ExpressionDataProcessor _processor
+
+    String studyName = 'TestSample'
+    String studyId = 'GSE0'
+
+    Sql getSql() {
+        return _sql ?: (_sql = Sql.newInstance(connectionSettings.jdbcConnectionString,
+                connectionSettings.password, connectionSettings.username,
+                connectionSettings.jdbcDriver))
+    }
+
+    ExpressionDataProcessor getProcessor() {
+        _processor ?: (_processor = new ExpressionDataProcessor([
+                logger        : new Logger([isInteractiveMode: true]),
+                db            : connectionSettings,
+                controlSchema : 'tm_cz',
                 securitySymbol: 'N'
-        ])
+        ]))
+    }
+
+    void assertThatSampleIsPresent(String sampleId) {
+        def sample = sql.firstRow("select * from deapp.de_subject_sample_mapping where trial_name = ? and sample_cd = ?",
+                studyId, sampleId)
+        assertThat(sample, notNullValue())
+    }
+
+    void testItLoadsData() {
         processor.process(
-                new File("Public Studies/TestSample_GSE0/ExpressionDataToUpload"),
-                [name: 'TestSample', node: 'Public Studies\\TestSample'])
+                new File("Public Studies/${studyName}_${studyId}/ExpressionDataToUpload"),
+                [name: studyName, node: "Public Studies\\${studyName}".toString()])
+        assertThatSampleIsPresent('GSM1000000719')
+    }
+
+    void testItMergeSamples() {
+        processor.process(
+                new File("Public Studies/${studyName}_${studyId}/ExpressionDataToUpload"),
+                [name: studyName, node: "Public Studies\\${studyName}".toString()])
+        assertThatSampleIsPresent('GSM1000000719')
+        processor.process(
+                new File("Additional Samples/${studyName}_${studyId}/ExpressionDataToUpload"),
+                [name: studyName, node: "Public Studies\\${studyName}".toString()])
+        assertThatSampleIsPresent('GSM2000000719')
+        assertThatSampleIsPresent('GSM1000000719')
     }
 }
