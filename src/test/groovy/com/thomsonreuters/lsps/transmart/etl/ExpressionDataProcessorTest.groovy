cec84a89c29bb7ee0c2b07b1ca73673b3a3c48ad
bondarev <alexander.i.bondarev@gmail.com>
2014-04-04 18:53:16 +0400

    add some more checks to expression data processor test

--- src/test/groovy/com/thomsonreuters/lsps/transmart/etl/ExpressionDataProcessorTest.groovy
+++ src/test/groovy/com/thomsonreuters/lsps/transmart/etl/ExpressionDataProcessorTest.groovy
@@ -1,5 +1,8 @@
 package com.thomsonreuters.lsps.transmart.etl
 
+import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasNode
+import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasPatient
+import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasSample
 import static org.hamcrest.CoreMatchers.equalTo
 import static org.hamcrest.CoreMatchers.notNullValue
 import static org.junit.Assert.assertThat
@@ -16,6 +19,14 @@ class ExpressionDataProcessorTest extends ConfigAwareTestCase {
 
     ExpressionDataProcessor getProcessor() {
         _processor ?: (_processor = new ExpressionDataProcessor(config))
+    }
+
+    @Override
+    void setUp() {
+        super.setUp()
+        sql.execute('delete from i2b2demodata.observation_fact where modifier_cd = ?', studyId)
+        sql.execute('delete from deapp.de_subject_sample_mapping where trial_name = ?', studyId)
+        runScript('I2B2_PROCESS_MRNA_DATA.sql')
     }
 
     void assertThatSampleIsPresent(String sampleId, sampleData) {
@@ -40,6 +51,10 @@ class ExpressionDataProcessorTest extends ConfigAwareTestCase {
         processor.process(
                 new File("fixtures/Test Studies/${studyName}_${studyId}/ExpressionDataToUpload"),
                 [name: studyName, node: "Test Studies\\${studyName}".toString()])
+        assertThat(db, hasSample(studyId, 'TST1000000719'))
+        assertThat(db, hasPatient('996RMS').inTrial(studyId))
+        assertThat(db, hasNode("\\Test Studies\\${studyName}\\Biomarker Data\\Test GEX Platform\\Blood\\").
+                withPatientCount(28))
         assertThatSampleIsPresent('TST1000000719', ['1007_s_at': 6.624529839])
     }
 
@@ -49,11 +64,18 @@ class ExpressionDataProcessorTest extends ConfigAwareTestCase {
                 [name: studyName, node: "Test Studies\\${studyName}".toString()])
         assertThatSampleIsPresent('TST1000000719', ['1007_s_at': 6.624529839])
         assertThatSampleIsPresent('TST1000000722', ['1007_s_at': 6.374219894])
+        assertThatSampleIsPresent('TST1000000723', ['1007_s_at': 6.653120041])
+        assertThat(db, hasNode("\\Test Studies\\${studyName}\\Biomarker Data\\Test GEX Platform\\Blood\\").
+                withPatientCount(29))
+
         processor.process(
                 new File("fixtures/Additional Test Studies/${studyName}_${studyId}/ExpressionDataToUpload"),
                 [name: studyName, node: "Test Studies\\${studyName}".toString()])
-        assertThatSampleIsPresent('TST2000000719', ['1007_s_at': 6.624529839])
         assertThatSampleIsPresent('TST1000000719', ['1007_s_at': 6.624529839])
+        assertThatSampleIsPresent('TST2000000719', ['1007_s_at': 7.624529839])
         assertThatSampleIsPresent('TST1000000722', ['1007_s_at': 5.374219894])
+        assertThatSampleIsPresent('TST1000000723', ['1007_s_at': 6.653120041])
+        assertThat(db, hasNode("\\Test Studies\\${studyName}\\Biomarker Data\\Test GEX Platform\\Blood\\").
+                withPatientCount(30))
     }
 }
