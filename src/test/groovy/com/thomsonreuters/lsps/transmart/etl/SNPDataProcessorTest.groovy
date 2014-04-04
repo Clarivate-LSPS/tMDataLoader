2277f342c9cc2db6e96218bb8dcf9a532a3ce8cf
bondarev <alexander.i.bondarev@gmail.com>
2014-04-04 18:21:02 +0400

    rewrite i2b2_process_snp_data using i2b2_load_samples

--- src/test/groovy/com/thomsonreuters/lsps/transmart/etl/SNPDataProcessorTest.groovy
+++ src/test/groovy/com/thomsonreuters/lsps/transmart/etl/SNPDataProcessorTest.groovy
@@ -3,6 +3,8 @@ package com.thomsonreuters.lsps.transmart.etl
 import static com.thomsonreuters.lsps.transmart.Fixtures.getAdditionalStudiesDir
 import static com.thomsonreuters.lsps.transmart.Fixtures.studyDir
 import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasNode
+import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasPatient
+import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasSample
 import static org.hamcrest.CoreMatchers.equalTo
 import static org.hamcrest.CoreMatchers.notNullValue
 import static org.junit.Assert.assertThat
@@ -22,6 +24,7 @@ class SNPDataProcessorTest extends ConfigAwareTestCase {
         super.setUp()
         sql.execute('delete from i2b2demodata.observation_fact where modifier_cd = ?', studyId)
         sql.execute('delete from deapp.de_subject_sample_mapping where trial_name = ?', studyId)
+        runScript('I2B2_PROCESS_SNP_DATA.sql')
     }
 
     SNPDataProcessor getProcessor() {
@@ -32,12 +35,8 @@ class SNPDataProcessorTest extends ConfigAwareTestCase {
         def sample = sql.firstRow('select * from deapp.de_subject_sample_mapping where trial_name = ? and sample_cd = ?',
                 studyId, sampleId)
         assertThat(sample, notNullValue())
-        String suffix = '';
-        if (sample.hasProperty("partition_id")) {
-            suffix = sample.partition_id ? "_${sample.partition_id}" : ''
-        }
         sampleData.each { probe_id, value ->
-            def rows = sql.rows("select d.log_intensity from deapp.de_subject_microarray_data${suffix} d " +
+            def rows = sql.rows("select d.log_intensity from deapp.de_subject_microarray_data d " +
                     "inner join deapp.de_mrna_annotation a on d.probeset_id = a.probeset_id " +
                     "where a.gpl_id = ? and d.assay_id = ? and a.probe_id = ?",
                     platformId, sample.assay_id, probe_id)
@@ -49,6 +48,9 @@ class SNPDataProcessorTest extends ConfigAwareTestCase {
     void testItLoadsData() {
         processor.process(new File(studyDir(studyName, studyId), "SNPDataToUpload"),
                 [name: studyName, node: "Test Studies\\${studyName}".toString()])
+        assertThat(db, hasSample(studyId, 'TST001'))
+        assertThat(db, hasPatient('Subject_0').inTrial(studyId))
+        assertThat(db, hasNode("\\Test Studies\\${studyName}\\SNP\\Test SNP Platform\\Unknown\\").withPatientCount(3))
         assertThatSampleIsPresent('TST001', ['SNP_A-4265338': 0.628913])
     }
 
