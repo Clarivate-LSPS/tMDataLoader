9d809f60ee6ac7b53759b0c2231091ed78635028
bondarev <alexander.i.bondarev@gmail.com>
2014-04-08 13:34:54 +0400

    add handful sql find method

--- src/main/groovy/com/thomsonreuters/lsps/transmart/sql/SqlMethods.groovy
+++ src/main/groovy/com/thomsonreuters/lsps/transmart/sql/SqlMethods.groovy
@@ -1,18 +1,33 @@
 package com.thomsonreuters.lsps.transmart.sql
 
+import groovy.sql.GroovyRowResult
 import groovy.sql.Sql
 
 /**
  * Created by bondarev on 4/7/14.
  */
+@Category(Sql)
 class SqlMethods {
-    static void callProcedure(Sql sql, String procedureName, Object... params) {
-        sql.call("{call ${procedureName}(${(['?'] * params.size()).join(',')})}", params.collect {
-            if (it instanceof GString) {
-                it as String
-            } else {
-                it
-            }
-        })
+    private static Object prepareValue(Object value) {
+        switch (value.class) {
+            case GString:
+                return value as String
+            case Calendar:
+                return prepareValue((value as Calendar).time)
+            case Date:
+                return new java.sql.Date((value as Date).time)
+            default:
+                return value
+        }
+    }
+
+    void callProcedure(String procedureName, Object... params) {
+        call("{call ${procedureName}(${(['?'] * params.size()).join(',')})}", params.collect { prepareValue(it) })
+    }
+
+    GroovyRowResult findRecord(Map<CharSequence, Object> attrs, CharSequence tableName) {
+        def columns = attrs.keySet()
+        firstRow("select * from ${tableName} where ${columns.collect { "${it}=?" }.join(' and ')}",
+                columns.collect { prepareValue(attrs[it]) })
     }
 }
