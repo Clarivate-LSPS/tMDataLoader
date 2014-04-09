16ca0095d062526712790e66d4645f09898c6235
bondarev <alexander.i.bondarev@gmail.com>
2014-04-09 12:33:45 +0400

    add de_variant_population_data & de_variant_population_info ddl

--- sql-postgres/5000_VCF_CREATE_TABLES.sql
+++ sql-postgres/5000_VCF_CREATE_TABLES.sql
@@ -192,5 +192,90 @@ IF NOT EXISTS (select 1 from pg_tables where schemaname = 'deapp' and tablename=
 
 END IF;
 
+IF NOT EXISTS (select 1 from pg_tables where schemaname = 'deapp' and tablename='de_variant_population_data') THEN
+  --
+  -- Name: deapp.de_variant_population_data_seq; Type: SEQUENCE; Schema: deapp; Owner: -
+  --
+  CREATE SEQUENCE deapp.de_variant_population_data_seq
+      START WITH 1
+      INCREMENT BY 1
+      NO MINVALUE
+      NO MAXVALUE
+      CACHE 1;
+
+  --
+  -- Name: deapp.de_variant_population_data; Type: TABLE; Schema: deapp; Owner: -
+  --
+  CREATE TABLE deapp.de_variant_population_data (
+      variant_population_data_id bigint DEFAULT nextval('deapp.de_variant_population_data_seq'::regclass) NOT NULL,
+      dataset_id character varying(50),
+      chr character varying(50),
+      pos bigint,
+      info_name character varying(100),
+      info_index integer DEFAULT 0,
+      integer_value bigint,
+      float_value double precision,
+      text_value character varying(5000)
+  );
+
+  --
+  -- Name: deapp.de_variant_population_data_id_idx; Type: CONSTRAINT; Schema: deapp; Owner: -
+  --
+  ALTER TABLE ONLY deapp.de_variant_population_data
+      ADD CONSTRAINT de_variant_population_data_id_idx PRIMARY KEY (variant_population_data_id);
+
+  --
+  -- Name: de_variant_population_data_default_idx; Type: INDEX; Schema: deapp; Owner: -
+  --
+  CREATE INDEX de_variant_population_data_default_idx ON deapp.de_variant_population_data USING btree (dataset_id, chr, pos, info_name);
+
+  --
+  -- Name: de_variant_population_data_fk; Type: FK CONSTRAINT; Schema: deapp; Owner: -
+  --
+  ALTER TABLE ONLY deapp.de_variant_population_data
+      ADD CONSTRAINT de_variant_population_data_fk FOREIGN KEY (dataset_id) REFERENCES deapp.de_variant_dataset(dataset_id);
+END IF;
+
+IF NOT EXISTS (select 1 from pg_tables where schemaname = 'deapp' and tablename='de_variant_population_info') THEN
+  --
+  -- Name: deapp.de_variant_population_info_seq; Type: SEQUENCE; Schema: deapp; Owner: -
+  --
+  CREATE SEQUENCE deapp.de_variant_population_info_seq
+      START WITH 1
+      INCREMENT BY 1
+      NO MINVALUE
+      NO MAXVALUE
+      CACHE 1;
+
+  --
+  -- Name: deapp.de_variant_population_info; Type: TABLE; Schema: deapp; Owner: -
+  --
+  CREATE TABLE deapp.de_variant_population_info (
+      variant_population_info_id bigint DEFAULT nextval('deapp.de_variant_population_info_seq'::regclass) NOT NULL,
+      dataset_id character varying(50),
+      info_name character varying(100),
+      description text,
+      type character varying(30),
+      number character varying(10)
+  );
+
+  --
+  -- Name: de_variant_population_info_id_idx; Type: CONSTRAINT; Schema: deapp; Owner: -
+  --
+  ALTER TABLE ONLY deapp.de_variant_population_info
+      ADD CONSTRAINT de_variant_population_info_id_idx PRIMARY KEY (variant_population_info_id);
+
+  --
+  -- Name: variant_population_info_dataset_name; Type: INDEX; Schema: deapp; Owner: -
+  --
+  CREATE INDEX variant_population_info_dataset_name ON deapp.de_variant_population_info USING btree (dataset_id, info_name);
+
+  --
+  -- Name: de_variant_population_info_fk; Type: FK CONSTRAINT; Schema: deapp; Owner: -
+  --
+  ALTER TABLE ONLY deapp.de_variant_population_info
+      ADD CONSTRAINT de_variant_population_info_fk FOREIGN KEY (dataset_id) REFERENCES deapp.de_variant_dataset(dataset_id);
+END IF;
+
 END;
 $$;
