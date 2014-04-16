b4d428d3f32fac4504ba446c6fdb932d9007fbbf
bondarev <alexander.i.bondarev@gmail.com>
2014-04-16 15:37:48 +0400

    fix SNPDataProcessor tests

--- src/test/groovy/com/thomsonreuters/lsps/transmart/etl/SNPDataProcessorTest.groovy
+++ src/test/groovy/com/thomsonreuters/lsps/transmart/etl/SNPDataProcessorTest.groovy
@@ -1,12 +1,10 @@
 package com.thomsonreuters.lsps.transmart.etl
 
+import com.thomsonreuters.lsps.transmart.sql.SqlMethods
+
 import static com.thomsonreuters.lsps.transmart.Fixtures.getAdditionalStudiesDir
 import static com.thomsonreuters.lsps.transmart.Fixtures.studyDir
-import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasNode
-import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasPatient
-import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasSample
-import static org.hamcrest.CoreMatchers.equalTo
-import static org.hamcrest.CoreMatchers.notNullValue
+import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.*
 import static org.junit.Assert.assertThat
 
 /**
@@ -17,7 +15,6 @@ class SNPDataProcessorTest extends ConfigAwareTestCase {
 
     String studyName = 'Test Study'
     String studyId = 'GSE0'
-    String platformId = 'TST_SNP'
 
     @Override
     void setUp() {
@@ -31,18 +28,16 @@ class SNPDataProcessorTest extends ConfigAwareTestCase {
         _processor ?: (_processor = new SNPDataProcessor(config))
     }
 
-    void assertThatSampleIsPresent(String sampleId, sampleData) {
-        def sample = sql.firstRow('select * from deapp.de_subject_sample_mapping where trial_name = ? and sample_cd = ?',
-                studyId, sampleId)
-        assertThat(sample, notNullValue())
-        sampleData.each { probe_id, value ->
-            def rows = sql.rows("select d.log_intensity from deapp.de_subject_microarray_data d " +
-                    "inner join deapp.de_mrna_annotation a on d.probeset_id = a.probeset_id " +
-                    "where a.gpl_id = ? and d.assay_id = ? and a.probe_id = ?",
-                    platformId, sample.assay_id, probe_id)
-            assertThat(rows?.size(), equalTo(1))
-            assertEquals(rows[0].log_intensity as double, value as double, 0.001)
+    def findSample(String sampleId) {
+        use(SqlMethods) {
+            db.findRecord('deapp.de_subject_sample_mapping', trial_name: studyId, platform: 'SNP', sample_cd: sampleId)
         }
+    }
+
+    def hasCopyNumber(String sampleId, String snpName, int copyNumber) {
+        def sample = findSample(sampleId)
+        hasRecord('deapp.de_snp_copy_number',
+                [patient_num: sample.patient_id, snp_name: snpName], [copy_number: copyNumber])
     }
 
     void testItLoadsData() {
@@ -51,21 +46,27 @@ class SNPDataProcessorTest extends ConfigAwareTestCase {
         assertThat(db, hasSample(studyId, 'TST001', platform: 'SNP'))
         assertThat(db, hasPatient('Subject_0').inTrial(studyId))
         assertThat(db, hasNode("\\Test Studies\\${studyName}\\SNP\\Test SNP Platform\\Unknown\\").withPatientCount(3))
-        assertThatSampleIsPresent('TST001', ['SNP_A-4265338': 0.628913])
+        def sample = findSample('TST001')
+        assertThat(db, hasRecord('deapp.de_snp_calls_by_gsm',
+                [patient_num: sample.patient_id, gsm_num: 'TST001', snp_name: 'SNP_A-1984209'],
+                [snp_calls: '2']))
+        assertThat(db, hasRecord('deapp.de_snp_copy_number',
+                [patient_num: sample.patient_id, snp_name: 'SNP_A-4265338'],
+                [chrom: '1', chrom_pos: 216721255, copy_number: 2]))
     }
 
     void testItMergeSamples() {
         processor.process(new File(studyDir(studyName, studyId), "SNPDataToUpload"),
                 [name: studyName, node: "Test Studies\\${studyName}".toString()])
-        assertThatSampleIsPresent('TST001', ['SNP_A-4265338': 0.628913])
-        assertThatSampleIsPresent('TST002', ['CN_497981': 0.057206])
+        assertThat(db, hasCopyNumber('TST001', 'SNP_A-4265338', 2))
+        assertThat(db, hasCopyNumber('TST002', 'CN_497981', 1))
         assertThat(sql, hasNode($/\Test Studies\${studyName}\SNP\Test SNP Platform\Unknown\/$).withPatientCount(3))
 
         processor.process(new File(studyDir(studyName, studyId, additionalStudiesDir), "SNPDataToUpload"),
                 [name: studyName, node: "Test Studies\\${studyName}".toString()])
-        assertThatSampleIsPresent('TST001', ['SNP_A-4265338': 0.528913])
-        assertThatSampleIsPresent('TST002', ['CN_497981': 0.057206])
-        assertThatSampleIsPresent('TST001', ['SNP_A-2176913': 0.018677])
+        assertThat(db, hasCopyNumber('TST001', 'SNP_A-4265338', 1))
+        assertThat(db, hasCopyNumber('TST002', 'CN_497981', 1))
+        assertThat(db, hasCopyNumber('TST001', 'SNP_A-2176913', 1))
         assertThat(sql, hasNode($/\Test Studies\${studyName}\SNP\Test SNP Platform\Unknown\/$).withPatientCount(4))
     }
 }
