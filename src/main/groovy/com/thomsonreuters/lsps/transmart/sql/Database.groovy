17428f4d700e6d4a5583231d8a02ddcabab516f5
mirasrael <d_signer@mail.ru>
2014-07-23 17:34:50 +0400

    Support host names with '.' and '-' in jdbc url

--- src/main/groovy/com/thomsonreuters/lsps/transmart/sql/Database.groovy
+++ src/main/groovy/com/thomsonreuters/lsps/transmart/sql/Database.groovy
@@ -52,8 +52,8 @@ class Database {
     }
 
     private void parseOracleJdbcConnectionString() {
-        def match = config.jdbcConnectionString =~ /^jdbc:oracle:thin:@(\w+)?(?::(\d+))?:(\w+)$/ ?:
-                config.jdbcConnectionString =~ /^jdbc:oracle:thin:@\/\/(\w+)?(?::(\d+))?\/(\w+)$/
+        def match = config.jdbcConnectionString =~ /^jdbc:oracle:thin:@((?:\w|[-.])+)?(?::(\d+))?:(\w+)$/ ?:
+                config.jdbcConnectionString =~ /^jdbc:oracle:thin:@\/\/((?:\w|[-.])+)?(?::(\d+))?\/(\w+)$/
         if (match.size()) {
             databaseType = DatabaseType.Oracle
             host = match[0][1] ?: 'localhost'
@@ -63,7 +63,7 @@ class Database {
     }
 
     private void parsePostgresJdbcConnectionString() {
-        def match = config.jdbcConnectionString =~ /^jdbc:postgresql:(?:\/\/(\w+)(?::(\d+))?\/)?(\w+)(?:\?.*)?$/
+        def match = config.jdbcConnectionString =~ /^jdbc:postgresql:(?:\/\/((?:\w|[-.])+)(?::(\d+))?\/)?(\w+)(?:\?.*)?$/
         if (match) {
             databaseType = DatabaseType.Postgres
             host = match[0][1] ?: 'localhost'
