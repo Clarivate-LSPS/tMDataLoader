a580c0ae3b20ef70939650920a041abab99ba309
mirasrael <d_signer@mail.ru>
2014-07-25 12:31:18 +0400

    Add migrations.sql, move procedures.sql for Postgres scripts (DDL update for tm data loader)

---
+++ sql/postgres/procedures.sql
@@ -0,0 +1,15 @@
+\i procedures/median.sql
+\i procedures/AUDIT_FUNCTIONS.sql
+\i procedures/I2B2_ADD_NODE.sql
+\i procedures/I2B2_CREATE_CONCEPT_COUNTS.sql
+\i procedures/I2B2_DELETE_1_NODE.sql
+\i procedures/I2B2_LOAD_SECURITY_DATA.sql
+\i procedures/I2B2_LOAD_SAMPLES.sql
+\i procedures/I2B2_PROCESS_MRNA_DATA.sql
+\i procedures/I2B2_CREATE_FULL_TREE.sql
+\i procedures/I2B2_LOAD_CLINICAL_DATA.sql
+\i procedures/I2B2_PROCESS_SNP_DATA.sql
+\i procedures/I2B2_PROCESS_VCF_DATA.sql
+\i procedures/I2B2_DELETE_ALL_DATA.sql
+\i procedures/I2B2_MOVE_STUDY.sql
+
