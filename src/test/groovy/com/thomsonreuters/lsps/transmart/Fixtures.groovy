efc9d7b6776146f72caa7e061660e9702f3dc89d
mirasrael <d_signer@mail.ru>
2014-07-31 15:11:13 +0400

    Multi VCF support

--- src/test/groovy/com/thomsonreuters/lsps/transmart/Fixtures.groovy
+++ src/test/groovy/com/thomsonreuters/lsps/transmart/Fixtures.groovy
@@ -20,6 +20,10 @@ class Fixtures {
         new File(studyDir(studyName, studyId), 'VCFDataToUpload')
     }
 
+    static File getMultipleVcfData(String studyName = 'Test Study', String studyId = 'GSE0') {
+        new File(studyDir(studyName, studyId), 'MultiVCFDataToUpload')
+    }
+
     static File getClinicalData(String studyName = 'Test Study', String studyId = 'GSE0') {
         new File(studyDir(studyName, studyId), 'ClinicalDataToUpload')
     }
