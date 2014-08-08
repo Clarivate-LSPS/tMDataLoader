05d9f17a14f4de2a892be63e7ee13832882ea264
mirasrael <d_signer@mail.ru>
2014-08-08 19:47:25 +0400

    Update permissions and synonyms

--- sql/oracle/2_permissions.sql
+++ sql/oracle/2_permissions.sql
@@ -1,10 +1,62 @@
 set define on;
-
 define TM_CZ_SCHEMA=TM_CZ;
+define TM_LZ_SCHEMA=TM_LZ;
+define TM_WZ_SCHEMA=TM_WZ;
 
-grant select, insert on i2b2metadata.table_access to "&TM_CZ_SCHEMA";
-grant select, insert, delete on i2b2demodata.observation_fact to "&TM_CZ_SCHEMA";
-grant select, insert, delete on i2b2demodata.concept_dimension to "&TM_CZ_SCHEMA";
-grant select, insert, delete on i2b2metadata.i2b2 to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on i2b2metadata.table_access to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on i2b2demodata.observation_fact to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on i2b2demodata.concept_dimension to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on i2b2demodata.concept_counts to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on i2b2demodata.patient_dimension to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on i2b2demodata.patient_trial to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on i2b2metadata.i2b2 to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on i2b2metadata.i2b2_tags to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on i2b2metadata.i2b2_secure to "&TM_CZ_SCHEMA";
+
+grant select, insert, update, delete on biomart.bio_data_uid to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on biomart.bio_experiment to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on biomart.bio_compound to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on biomart.bio_content to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on biomart.bio_data_compound to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on biomart.bio_data_disease to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on biomart.bio_data_taxonomy to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on biomart.bio_disease to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on biomart.bio_clinical_trial to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on biomart.bio_content_reference to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on biomart.bio_content_repository to "&TM_CZ_SCHEMA";
+
+grant select, insert, update, delete on deapp.de_subject_sample_mapping to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on deapp.de_subject_microarray_data to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on deapp.de_snp_data_dataset_loc to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on deapp.de_snp_data_by_patient to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on deapp.de_snp_copy_number to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on deapp.de_snp_subject_sorted_def to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on deapp.de_snp_data_by_probe to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on deapp.de_subject_snp_dataset to "&TM_CZ_SCHEMA";
+
+grant select, insert, update, delete on deapp.de_variant_subject_summary to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on deapp.de_variant_population_data to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on deapp.de_variant_population_info to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on deapp.de_variant_subject_detail to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on deapp.de_variant_subject_idx to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on deapp.de_variant_dataset to "&TM_CZ_SCHEMA";
+
+grant select, insert, update, delete on searchapp.search_secure_object to "&TM_CZ_SCHEMA";
+
+grant select, insert, update, delete on "&TM_LZ_SCHEMA".lz_src_clinical_data to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on "&TM_LZ_SCHEMA".lt_src_study_metadata to "&TM_CZ_SCHEMA";
+
+grant select, insert, update, delete on "&TM_WZ_SCHEMA".wt_trial_nodes to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on "&TM_WZ_SCHEMA".wrk_clinical_data to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on "&TM_WZ_SCHEMA".wt_num_data_types to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on "&TM_WZ_SCHEMA".wt_clinical_data_dups to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on "&TM_WZ_SCHEMA".wt_subject_mrna_probeset to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on "&TM_WZ_SCHEMA".wt_subject_microarray_logs to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on "&TM_WZ_SCHEMA".wt_subject_microarray_calcs to "&TM_CZ_SCHEMA";
+grant select, insert, update, delete on "&TM_WZ_SCHEMA".wt_subject_microarray_med to "&TM_CZ_SCHEMA";
+
+grant select on sys.all_tables to "&TM_CZ_SCHEMA";
+
 grant select on i2b2metadata.i2b2_id_seq to "&TM_CZ_SCHEMA";
 grant select on i2b2demodata.concept_id to "&TM_CZ_SCHEMA";
+grant select on i2b2demodata.seq_patient_num to "&TM_CZ_SCHEMA";
---
+++ sql/oracle/3_synonyms.sql
@@ -0,0 +1,21 @@
+set define on;
+define TM_CZ_SCHEMA=TM_CZ;
+define TM_LZ_SCHEMA=TM_LZ;
+define TM_WZ_SCHEMA=TM_WZ;
+
+create or replace synonym "&TM_CZ_SCHEMA".lz_src_clinical_data for "&TM_LZ_SCHEMA".lz_src_clinical_data;
+create or replace synonym "&TM_CZ_SCHEMA".lt_src_study_metadata for "&TM_LZ_SCHEMA".lt_src_study_metadata;
+create or replace synonym "&TM_CZ_SCHEMA".lt_src_mrna_subj_samp_map for "&TM_LZ_SCHEMA".lt_src_mrna_subj_samp_map;
+create or replace synonym "&TM_CZ_SCHEMA".lt_snp_copy_number for "&TM_LZ_SCHEMA".lt_snp_copy_number;
+create or replace synonym "&TM_CZ_SCHEMA".lt_snp_calls_by_gsm for "&TM_LZ_SCHEMA".lt_snp_calls_by_gsm;
+
+create or replace synonym "&TM_CZ_SCHEMA".wt_trial_nodes for "&TM_WZ_SCHEMA".wt_trial_nodes;
+create or replace synonym "&TM_CZ_SCHEMA".wrk_clinical_data for "&TM_WZ_SCHEMA".wrk_clinical_data;
+create or replace synonym "&TM_CZ_SCHEMA".wt_num_data_types for "&TM_WZ_SCHEMA".wt_num_data_types;
+create or replace synonym "&TM_CZ_SCHEMA".wt_clinical_data_dups for "&TM_WZ_SCHEMA".wt_clinical_data_dups;
+create or replace synonym "&TM_CZ_SCHEMA".wt_subject_mrna_probeset for "&TM_WZ_SCHEMA".wt_subject_mrna_probeset;
+create or replace synonym "&TM_CZ_SCHEMA".wt_subject_microarray_logs for "&TM_WZ_SCHEMA".wt_subject_microarray_logs;
+create or replace synonym "&TM_CZ_SCHEMA".wt_subject_microarray_calcs for "&TM_WZ_SCHEMA".wt_subject_microarray_calcs;
+create or replace synonym "&TM_CZ_SCHEMA".wt_subject_microarray_med for "&TM_WZ_SCHEMA".wt_subject_microarray_med;
+create or replace synonym "&TM_CZ_SCHEMA".wt_mrna_nodes for "&TM_WZ_SCHEMA".wt_mrna_nodes;
+create or replace synonym "&TM_CZ_SCHEMA".wt_mrna_node_values for "&TM_WZ_SCHEMA".wt_mrna_node_values;
