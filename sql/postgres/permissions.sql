d1a07eb1359a8547f6ae9ef952a31bff70924c0d
mirasrael <d_signer@mail.ru>
2014-09-09 16:35:58 +0400

    Add grant execute permission to tm_cz user on tm_cz schema functions

--- sql/postgres/permissions.sql
+++ sql/postgres/permissions.sql
@@ -23,6 +23,8 @@ grant usage on sequence deapp.de_variant_population_data_seq to tm_cz;
 grant usage on sequence deapp.de_variant_subject_summary_seq to tm_cz;
 grant usage on sequence deapp.de_variant_subject_detail_seq to tm_cz;
 grant select on deapp.de_subject_sample_mapping to tm_cz;
+grant execute on all functions in schema tm_cz to tm_cz;
+grant usage on schema tm_cz to tm_cz;
 
 -- Permissions for biomart_user
 grant select on deapp.de_variant_dataset to biomart_user;
