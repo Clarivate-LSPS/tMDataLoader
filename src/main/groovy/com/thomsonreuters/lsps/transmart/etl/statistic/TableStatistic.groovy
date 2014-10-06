0380830370d050a0d9484b7a86d0e1877efbccef
mirasrael <d_signer@mail.ru>
2014-10-06 17:17:52 +0400

    Record statistic for ID variable

---
+++ src/main/groovy/com/thomsonreuters/lsps/transmart/etl/statistic/TableStatistic.groovy
@@ -0,0 +1,59 @@
+package com.thomsonreuters.lsps.transmart.etl.statistic
+
+import com.sun.javaws.exceptions.InvalidArgumentException
+
+/**
+ * Date: 06.10.2014
+ * Time: 14:58
+ */
+class TableStatistic {
+    VariableStatistic idVariable
+    def variables = [:]
+
+    private Map currentVariableValues
+
+    TableStatistic withRecordStatisticForVariable(String name, VariableType variableType) {
+        VariableStatistic variable = new VariableStatistic(name: name, type: variableType)
+        if (variableType == VariableType.ID) {
+            if (idVariable != null) {
+                throw new UnsupportedOperationException("Can't set ID variable to '${name}'. Another ID variable is already defined ('${idVariable.name}')")
+            }
+            idVariable = variable
+        }
+        variables.put(name, variable)
+        return this
+    }
+
+    def startCollectForRecord() {
+        if (idVariable.is(null)) {
+            throw new UnsupportedOperationException('No ID variable is defined')
+        }
+        if (!currentVariableValues.is(null)) {
+            throw new UnsupportedOperationException('Another record collecting is already in progress');
+        }
+        currentVariableValues = [:]
+    }
+
+    def collectVariableValue(String name, String value) {
+        currentVariableValues.put(name, value)
+    }
+
+    def endCollectForRecord() {
+        if (currentVariableValues.is(null)) {
+            throw new UnsupportedOperationException('No active collectForRecord operation in progress')
+        }
+        collectForRecord(currentVariableValues)
+        currentVariableValues = null
+    }
+
+    def collectForRecord(Map variableValues) {
+        String id = variableValues.get(idVariable.name)
+        variableValues.each { String name, String value ->
+            VariableStatistic var = variables.get(name)
+            if (var.is(null)) {
+                throw new InvalidArgumentException("Unknown variable: ${name}")
+            }
+            var.collectValue(id, value)
+        }
+    }
+}
