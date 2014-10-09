6edc98fa8aaa106cad88c98a56ffc51441374b4c
mirasrael <d_signer@mail.ru>
2014-10-09 19:37:27 +0400

    Parse range rules

--- src/main/groovy/com/thomsonreuters/lsps/transmart/etl/statistic/ValidationRule.groovy
+++ src/main/groovy/com/thomsonreuters/lsps/transmart/etl/statistic/ValidationRule.groovy
@@ -3,6 +3,9 @@ package com.thomsonreuters.lsps.transmart.etl.statistic
 import com.thomsonreuters.lsps.transmart.etl.LogType
 import com.thomsonreuters.lsps.transmart.etl.Logger
 
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 /**
  * Date: 07.10.2014
  * Time: 17:32
@@ -11,9 +14,11 @@ class ValidationRule {
     private static final Logger logger = Logger.getLogger(ValidationRule.class)
 
     final ValidationRuleType type
+    final String description
 
-    ValidationRule(ValidationRuleType type) {
+    ValidationRule(ValidationRuleType type, String description) {
         this.type = type
+        this.description = description
     }
 
     public static List<ValidationRule> parseList(String validationRules) {
@@ -23,16 +28,58 @@ class ValidationRule {
         }
         def tokenizer = new StringTokenizer(validationRules, ';')
         while (tokenizer.hasMoreTokens()) {
-            rules.add(parse(tokenizer.nextToken()))
+            ValidationRule rule = parse(tokenizer.nextToken())
+            if (rule) {
+                rules.add(rule)
+            }
         }
         return rules
     }
 
+    private static final Pattern greaterThanOrEqualTo = Pattern.compile(/^(?:greater than or equal to\s+|>=)\s*(.+)$/,
+            Pattern.CASE_INSENSITIVE)
+    private static final Pattern greaterThan = Pattern.compile(/^(?:greater than\s+|>)\s*(.+)$/, Pattern.CASE_INSENSITIVE)
+    private static final Pattern lesserThanOrEqualTo = Pattern.compile(/^(?:lesser than or equal to\s+|<=)\s*(.+)$/,
+            Pattern.CASE_INSENSITIVE)
+    private static final Pattern lesserThan = Pattern.compile(/^(?:lesser than\s+|<)\s*(.+)$/, Pattern.CASE_INSENSITIVE)
+    private static final Pattern between = Pattern.compile(/^between\s+(.+?)\s+to\s+(.+)$/)
+    private static final Pattern range = Pattern.compile(/^(.+?)-(.+)$/)
+
+    private static final Map<Pattern, Closure<ValueRange<String>>> valueRangeFactory = [
+            (greaterThanOrEqualTo): { Matcher m -> new ValueRange<>(from: m.group(1), includeFrom: true) },
+            (greaterThan)         : { Matcher m -> new ValueRange<>(from: m.group(1), includeFrom: false) },
+            (lesserThanOrEqualTo) : { Matcher m -> new ValueRange<>(to: m.group(1), includeTo: true) },
+            (lesserThan)          : { Matcher m -> new ValueRange<>(to: m.group(1), includeTo: false) },
+            (between)             : { Matcher m -> new ValueRange<>(from: m.group(1), to: m.group(2)) },
+            (range)               : { Matcher m -> new ValueRange<>(from: m.group(1), to: m.group(2)) },
+    ]
+
     public static ValidationRule parse(String validationRule) {
+        validationRule = validationRule.trim()
         if (validationRule.toLowerCase().equals('required')) {
-            return new ValidationRule(ValidationRuleType.Required)
-        } else {
-            logger.log(LogType.WARNING, "Can't parse validation rule: '${validationRule}', ignored")
+            return new ValidationRule(ValidationRuleType.Required, validationRule)
         }
+        ValueRange<String> valueRange
+        valueRangeFactory.each { pattern, creator ->
+            Matcher matcher = pattern.matcher(validationRule)
+            if (matcher.find()) {
+                valueRange = creator(matcher)
+            }
+        }
+        if (valueRange) {
+            //TODO: cast range to specific type
+            ValueRange<Double> targetRange = new ValueRange<Double>()
+            if (valueRange.from) {
+                targetRange.from = Double.parseDouble(valueRange.from)
+                targetRange.includeFrom = valueRange.includeFrom
+            }
+            if (valueRange.to) {
+                targetRange.to = Double.parseDouble(valueRange.to)
+                targetRange.includeTo = valueRange.includeTo
+            }
+            return new RangeValidationRule(validationRule, targetRange)
+        }
+        logger.log(LogType.WARNING, "Can't parse validation rule: '${validationRule}', ignored")
+        return null
     }
 }
