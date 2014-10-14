5df203ff7d35e4455ce46ee53c73aa8cc6f1d756
Valeria Hohlova <khoulwa.khoulwa@gmail.com>
2014-10-14 07:31:51 -0700

    Test for RBM datatype updated.

--- src/test/groovy/com/thomsonreuters/lsps/transmart/etl/RBMDataProcessorTest.groovy
+++ src/test/groovy/com/thomsonreuters/lsps/transmart/etl/RBMDataProcessorTest.groovy
@@ -1,5 +1,13 @@
 package com.thomsonreuters.lsps.transmart.etl
 
+import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasNode
+import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasPatient
+import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasRecord
+import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.hasSample
+import static org.hamcrest.CoreMatchers.equalTo
+import static org.hamcrest.CoreMatchers.notNullValue
+import static org.junit.Assert.assertThat
+
 class RBMDataProcessorTest extends GroovyTestCase implements ConfigAwareTestCase {
     private RBMDataProcessor _processor
     String studyName = 'Test RBM Study'
@@ -19,10 +27,38 @@ class RBMDataProcessorTest extends GroovyTestCase implements ConfigAwareTestCase
         runScript('I2B2_RBM_ZSCORE_CALC_NEW.sql')
     }
 
+    void assertThatSampleIsPresent(String sampleId, sampleData) {
+        def sample = sql.firstRow('select * from deapp.de_subject_sample_mapping where trial_name = ? and sample_cd = ?',
+                studyId, sampleId)
+        assertThat(sample, notNullValue())
+        String suffix = '';
+        if (sample.hasProperty("partition_id")){
+            suffix = sample.partition_id ? "_${sample.partition_id}" : ''
+        }
+        sampleData.each { probe_id, value ->
+            def rows = sql.rows("select d.zscore from deapp.de_subject_rbm_data${suffix} d " +
+                    "inner join deapp.de_rbm_annotation a on d.antigen_name = a.antigen_name " +
+                    "where a.gpl_id = ? and d.assay_id = ? and a.uniprot_id = ?",
+                    platformId, sample.assay_id, probe_id)
+            assertThat(rows?.size(), equalTo(1))
+            assertEquals(rows[0].zscore as double, value as double, 0.001)
+        }
+    }
+
 
     void testItLoadsData() {
         processor.process(
                 new File("fixtures/Test Studies/${studyName}/RBMDataToUpload"),
                 [name: studyName, node: "Test Studies\\${studyName}".toString()])
+        assertThat(db, hasSample(studyId, 'GA8015ZS-06'))
+        assertThat(db, hasPatient('1:S57023').inTrial(studyId))
+        assertThat(db, hasNode("\\Test Studies\\${studyName}\\Biomarker Data\\Test RBM Platform\\Intestine\\Test\\").
+                withPatientCount(2))
+
+        assertThat(db, hasRecord('deapp.de_subject_sample_mapping',
+                [trial_name: studyId, gpl_id: platformId, subject_id: 'S57024', sample_cd: 'GA8015ZS-06'],
+                [platform: 'RBM', site_id : '2']))
+
+        assertThatSampleIsPresent('GA8015ZS-06', ['P15514': -1])
     }
 }
