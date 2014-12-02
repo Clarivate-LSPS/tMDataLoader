cca03210d8c5f975206cb76a5af2d8cecdd73b55
Valeria Hohlova <khoulwa.khoulwa@gmail.com>
2014-12-02 16:14:05 +0400

    #4 - SerialHDD processor added to directory processor.

--- src/main/groovy/com/thomsonreuters/lsps/transmart/etl/DirectoryProcessor.groovy
+++ src/main/groovy/com/thomsonreuters/lsps/transmart/etl/DirectoryProcessor.groovy
@@ -35,6 +35,7 @@ class DirectoryProcessor {
             Protein     : ProteinDataProcessor,
             Metabolomics: MetabolomicsDataProcessor,
             RNASeq      : RNASeqDataProcessor,
+            SerialHDD   : SerialHDDDataProcessor,
     ]
 
     DirectoryProcessor(conf) {
