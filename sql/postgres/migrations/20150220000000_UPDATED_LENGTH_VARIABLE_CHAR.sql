fb52854edf2d34e82c6126be7ed147cac5f08aeb
Alexander Omelchenko <al.omelchenko@gmail.com>
2015-02-27 03:26:52 -0800

    Issue #26 Error when loading gene expression. Change length column fro load platform annotation

--- sql/postgres/migrations/20150220000000_UPDATED_LENGTH_VARIABLE_CHAR.sql
+++ sql/postgres/migrations/20150220000000_UPDATED_LENGTH_VARIABLE_CHAR.sql
@@ -1,8 +1,18 @@
-ALTER TABLE tm_lz.lt_src_deapp_annot
-   ALTER COLUMN gene_symbol TYPE character varying(200);
+ALTER TABLE tm_dataloader.lt_src_deapp_annot
+   ALTER COLUMN gene_symbol TYPE character varying(400);
 
 ALTER TABLE tm_cz.annotation_deapp
-   ALTER COLUMN gene_symbol TYPE character varying(200);
+   ALTER COLUMN gene_symbol TYPE character varying(400);
 
 ALTER TABLE deapp.de_mrna_annotation
-   ALTER COLUMN gene_symbol TYPE character varying(200);
+   ALTER COLUMN gene_symbol TYPE character varying(400);
+
+ALTER TABLE tm_dataloader.lt_src_deapp_annot
+   ALTER COLUMN probe_id TYPE character varying(200);
+
+ALTER TABLE deapp.de_mrna_annotation
+   ALTER COLUMN probe_id TYPE character varying(200);
+
+ALTER TABLE tm_cz.annotation_deapp
+   ALTER COLUMN probe_id TYPE character varying(200);
+
