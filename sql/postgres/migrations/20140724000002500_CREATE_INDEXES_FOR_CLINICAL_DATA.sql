cdbff048956be6951484860be90fdf2f9ff7a873
Alexander Omelchenko <al.omelchenko@gmail.com>
2015-03-24 01:54:47 -0700

    Support aCGH: Added index for de_chromosomal_region table.

--- sql/postgres/migrations/20140724000002500_CREATE_INDEXES_FOR_CLINICAL_DATA.sql
+++ sql/postgres/migrations/20140724000002500_CREATE_INDEXES_FOR_CLINICAL_DATA.sql
@@ -97,3 +97,13 @@ DO $$
         END;
     END;
 $$;
+
+DO $$
+    BEGIN
+        BEGIN
+            CREATE INDEX IDX_DE_CHROMOSAL_REGION ON DEAPP.de_chromosomal_region (GPL_ID, GENE_SYMBOL);
+        EXCEPTION
+            WHEN duplicate_table THEN RAISE NOTICE 'Index IDX_DE_CHROMOSAL_REGION already exists.';
+        END;
+    END;
+$$;
