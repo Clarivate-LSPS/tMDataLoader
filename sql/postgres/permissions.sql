b08ee113e72e2d2019707a283cc044f7094f00fd
Alexander Bondarev <alexander.bondarev@thomsonreuters.com>
2015-04-14 16:00:54 +0300

    Update permissions script for PostgreSQL.

--- sql/postgres/permissions.sql
+++ sql/postgres/permissions.sql
@@ -33,8 +33,15 @@ grant usage on sequence deapp.de_variant_subject_detail_seq to tm_dataloader;
 grant select on deapp.de_subject_sample_mapping to tm_dataloader;
 
 grant usage on schema i2b2demodata to tm_dataloader;
-grant usage on schema i2b2metadata to tm_dataloader;
 grant usage on schema deapp to tm_dataloader;
+
+-- tm_cz
+grant all on tm_cz.cz_job_audit, tm_cz.cz_job_master, tm_cz.cz_job_error to tm_dataloader;
+grant usage on schema tm_cz to tm_dataloader;
+
+-- i2b2metadata
+grant usage on schema i2b2metadata to tm_dataloader;
+grant select on i2b2metadata.i2b2 to tm_dataloader;
 
 -- Permissions for biomart_user
 grant select on deapp.de_variant_dataset to biomart_user;
