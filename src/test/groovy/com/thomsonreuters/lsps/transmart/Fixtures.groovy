9423c278f8a002d22fdf209fdb89f812cfe41c0a
Alexander Bondarev <alexander.bondarev@thomsonreuters.com>
2015-04-27 13:51:15 +0300

    ExpressionDataProcessor test refactoring

--- src/test/groovy/com/thomsonreuters/lsps/transmart/Fixtures.groovy
+++ src/test/groovy/com/thomsonreuters/lsps/transmart/Fixtures.groovy
@@ -1,22 +1,46 @@
 package com.thomsonreuters.lsps.transmart
 
 import com.thomsonreuters.lsps.transmart.fixtures.ExpressionData
+import com.thomsonreuters.lsps.transmart.fixtures.StudyInfo
 import org.codehaus.groovy.runtime.DefaultGroovyMethods
 
 /**
  * Created by bondarev on 4/3/14.
  */
 class Fixtures {
-    static File getStudiesDir() {
-        return new File(['fixtures', 'Test Studies'].join(File.separator))
+    static class StudyDirFile extends File {
+        StudyInfo studyInfo
+
+        StudyDirFile(File parent, String studyId, String studyName) {
+            super(parent, "${studyName}_${studyId}")
+            this.studyInfo = new StudyInfo(studyId, studyName)
+        }
+
+        ExpressionData getExpressionData(String expressionDataFolder = 'ExpressionDataToUpload') {
+            return new ExpressionData(studyInfo: studyInfo, dir: new File(this, expressionDataFolder))
+        }
     }
 
-    static File getAdditionalStudiesDir() {
-        return new File(['fixtures', 'Additional Test Studies'].join(File.separator))
+    static class StudiesDirFile extends File {
+        StudiesDirFile(String path) {
+            super(path)
+        }
+
+        StudyDirFile studyDir(String studyName, String studyId) {
+            return new StudyDirFile(this, studyId, studyName)
+        }
     }
 
-    static File studyDir(String studyName, String studyId, File studiesDir = Fixtures.studiesDir) {
-        return new File(studiesDir, "${studyName}_${studyId}")
+    static StudiesDirFile getStudiesDir() {
+        return new StudiesDirFile(['fixtures', 'Test Studies'].join(File.separator))
+    }
+
+    static StudiesDirFile getAdditionalStudiesDir() {
+        return new StudiesDirFile(['fixtures', 'Additional Test Studies'].join(File.separator))
+    }
+
+    static StudyDirFile studyDir(String studyName, String studyId, File studiesDir = Fixtures.studiesDir) {
+        return new StudyDirFile(studiesDir, studyId, studyName)
     }
 
     static File getVcfData(String studyName = 'Test Study', String studyId = 'GSE0') {
@@ -28,8 +52,7 @@ class Fixtures {
     }
 
     static ExpressionData getExpressionData(String studyName = 'Test Study', String studyId = 'GSE0') {
-        new ExpressionData(studyId: studyId, studyName: studyName,
-                dir: new File(studyDir(studyName, studyId), 'ExpressionDataToUpload'))
+        return studyDir(studyName, studyId).expressionData
     }
 
     static File getClinicalData(String studyName = 'Test Study', String studyId = 'GSE0') {
---
+++ src/test/groovy/com/thomsonreuters/lsps/transmart/fixtures/StudyInfo.groovy
@@ -0,0 +1,15 @@
+package com.thomsonreuters.lsps.transmart.fixtures
+
+/**
+ * Date: 27.04.2015
+ * Time: 13:03
+ */
+class StudyInfo {
+    String id
+    String name
+
+    StudyInfo(String id, String name) {
+        this.id = id
+        this.name = name
+    }
+}
