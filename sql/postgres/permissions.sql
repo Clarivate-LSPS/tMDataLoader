0e39f106ee5abf3a39172d704620c9080a6ecdc2
Alexander Bondarev <alexander.bondarev@thomsonreuters.com>
2015-06-08 11:38:34 +0300

    Fix tm_dataloader postgresql permissions

--- sql/postgres/permissions.sql
+++ sql/postgres/permissions.sql
@@ -14,8 +14,11 @@ grant truncate, select, insert, delete on tm_dataloader.lt_src_mrna_xml_data to 
 -- grant insert, delete, select on deapp.de_snp_gene_map to tm_dataloader;
 -- grant insert, delete, select on deapp.de_snp_calls_by_gsm to tm_dataloader;
 -- grant insert, delete, select on deapp.de_snp_copy_number to tm_dataloader;
+
+-- deapp
 grant usage on schema deapp to tm_dataloader;
--- grant select, insert on deapp.de_gpl_info to tm_dataloader;
+grant select on deapp.de_mrna_annotation to tm_dataloader;
+grant select, insert on deapp.de_gpl_info to tm_dataloader;
 grant insert, delete, select on deapp.de_variant_dataset to tm_dataloader;
 grant insert, delete, select on deapp.de_variant_population_data to tm_dataloader;
 grant insert, delete, select on deapp.de_variant_population_info to tm_dataloader;
@@ -42,6 +45,10 @@ grant usage on schema tm_cz to tm_dataloader;
 -- i2b2metadata
 grant usage on schema i2b2metadata to tm_dataloader;
 grant select on i2b2metadata.i2b2 to tm_dataloader;
+
+-- i2b2demodata
+grant usage on schema i2b2demodata to tm_dataloader;
+grant select on i2b2demodata.concept_dimension to tm_dataloader;
 
 -- Permissions for biomart_user
 grant select on deapp.de_variant_dataset to biomart_user;
