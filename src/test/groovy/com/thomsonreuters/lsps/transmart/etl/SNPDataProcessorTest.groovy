cfd7c33d6471edaa4e2d4afdadaa10d219d47804
Alexander Bondarev <alexander.bondarev@thomsonreuters.com>
2015-07-07 16:41:03 +0300

    Update SNP upload procedures to load in de_sample_snp_data

--- src/test/groovy/com/thomsonreuters/lsps/transmart/etl/SNPDataProcessorTest.groovy
+++ src/test/groovy/com/thomsonreuters/lsps/transmart/etl/SNPDataProcessorTest.groovy
@@ -1,12 +1,9 @@
 package com.thomsonreuters.lsps.transmart.etl
 
-import com.thomsonreuters.lsps.transmart.sql.SqlMethods
-
 import static com.thomsonreuters.lsps.transmart.Fixtures.getAdditionalStudiesDir
 import static com.thomsonreuters.lsps.transmart.Fixtures.studyDir
 import static com.thomsonreuters.lsps.transmart.etl.matchers.SqlMatchers.*
 import static org.junit.Assert.assertThat
-
 /**
  * Created by bondarev on 2/24/14.
  */
@@ -21,24 +18,15 @@ class SNPDataProcessorTest extends GroovyTestCase implements ConfigAwareTestCase
     void setUp() {
         ConfigAwareTestCase.super.setUp()
         runScript('I2B2_PROCESS_SNP_DATA.sql')
-        new DeleteDataProcessor(config).process(['id'  : studyId,
-                                                 'path': "\\Test Studies\\${studyName}\\SNP\\"])
+        new DeleteDataProcessor(config).process(['id': studyId])
     }
 
     SNPDataProcessor getProcessor() {
         _processor ?: (_processor = new SNPDataProcessor(config))
     }
 
-    def findSample(String sampleId) {
-        use(SqlMethods) {
-            db.findRecord('deapp.de_subject_sample_mapping', trial_name: studyId, platform: 'SNP', sample_cd: sampleId)
-        }
-    }
-
-    def hasCopyNumber(String sampleId, String snpName, int copyNumber) {
-        def sample = findSample(sampleId)
-        hasRecord('deapp.de_snp_copy_number',
-                [patient_num: sample.patient_id, snp_name: snpName], [copy_number: copyNumber])
+    def hasCopyNumber(String sampleId, String snpName, double copyNumber) {
+        hasRecord('deapp.de_sample_snp_data', [sample_id: sampleId, snp_name: snpName], [copy_number: copyNumber])
     }
 
     void testItLoadsData() {
@@ -49,13 +37,12 @@ class SNPDataProcessorTest extends GroovyTestCase implements ConfigAwareTestCase
         assertThat(db, hasSample(studyId, 'TST001', platform: 'SNP'))
         assertThat(db, hasPatient('Subject_0').inTrial(studyId))
         assertThat(db, hasNode("\\Test Studies\\${studyName}\\SNP\\${platformId}\\Unknown\\").withPatientCount(3))
-        def sample = findSample('TST001')
-        assertThat(db, hasRecord('deapp.de_snp_calls_by_gsm',
-                [patient_num: sample.patient_id, gsm_num: 'TST001', snp_name: 'SNP_A-1984209'],
-                [snp_calls: '2']))
-        assertThat(db, hasRecord('deapp.de_snp_copy_number',
-                [patient_num: sample.patient_id, snp_name: 'SNP_A-4265338'],
-                [chrom: '1', chrom_pos: 216721255, copy_number: 2]))
+        assertThat(db, hasRecord('deapp.de_sample_snp_data',
+                [sample_id: 'TST001', snp_name: 'SNP_A-4265338'],
+                [snp_calls: null, copy_number: 0.628913]))
+        assertThat(db, hasRecord('deapp.de_sample_snp_data',
+                [sample_id: 'TST001', snp_name: 'SNP_A-1984209'],
+                [snp_calls: '2', copy_number: -0.187262]))
     }
 
     void testItMergeSamples() {
@@ -63,15 +50,15 @@ class SNPDataProcessorTest extends GroovyTestCase implements ConfigAwareTestCase
             processor.process(new File(studyDir(studyName, studyId), "SNPDataToUpload"),
                     [name: studyName, node: "Test Studies\\${studyName}".toString()])
         }
-        assertThat(db, hasCopyNumber('TST001', 'SNP_A-4265338', 2))
-        assertThat(db, hasCopyNumber('TST002', 'CN_497981', 1))
+        assertThat(db, hasCopyNumber('TST001', 'SNP_A-4265338', 0.628913))
+        assertThat(db, hasCopyNumber('TST002', 'CN_497981', 0.057206))
         assertThat(db, hasNode($/\Test Studies\${studyName}\SNP\${platformId}\Unknown\/$).withPatientCount(3))
 
         processor.process(new File(studyDir(studyName, studyId, additionalStudiesDir), "SNPDataToUpload"),
                 [name: studyName, node: "Test Studies\\${studyName}".toString()])
-        assertThat(db, hasCopyNumber('TST001', 'SNP_A-4265338', 1))
-        assertThat(db, hasCopyNumber('TST002', 'CN_497981', 1))
-        assertThat(db, hasCopyNumber('TST001', 'SNP_A-2176913', 1))
+        assertThat(db, hasCopyNumber('TST001', 'SNP_A-4265338', 0.528913))
+        assertThat(db, hasCopyNumber('TST002', 'CN_497981', 0.057206))
+        assertThat(db, hasCopyNumber('TST001', 'SNP_A-2176913', 0.018677))
         assertThat(db, hasNode($/\Test Studies\${studyName}\SNP\${platformId}\Unknown\/$).withPatientCount(4))
     }
 }
