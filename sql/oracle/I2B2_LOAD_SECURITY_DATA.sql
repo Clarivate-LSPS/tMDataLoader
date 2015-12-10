b2fe2b97e281cd3483d53abaf020faa8a363c383
Alexander <alexander.omelchenko@thomsonreuters.com>
2015-12-10 16:50:51 +0400

    Downgrade oracle procedure to default version

--- sql/oracle/I2B2_LOAD_SECURITY_DATA.sql
+++ sql/oracle/I2B2_LOAD_SECURITY_DATA.sql
@@ -1,12 +1,26 @@
-create or replace
-PROCEDURE         "I2B2_LOAD_SECURITY_DATA" 
+--
+-- Type: PROCEDURE; Owner: TM_CZ; Name: I2B2_LOAD_SECURITY_DATA
+--
+  CREATE OR REPLACE PROCEDURE "I2B2_LOAD_SECURITY_DATA"
 (
   currentJobID NUMBER := null
-)
+) AUTHID CURRENT_USER
 AS
-  -- JEA@20111206	Changed to use security token (tval_char) from SECURITY concept in observation_fact
-  -- JEA@20111221	Added distinct to subselect to fix duplicate record issue
-  -- JEA@20120214	Added coalesce to EXP:PUBLIC where no SECURITY records found (upper-level nodes)
+/*************************************************************************
+* Copyright 2008-2012 Janssen Research & Development, LLC.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+******************************************************************/
 
   --Audit variables
   newJobFlag INTEGER(1);
@@ -14,6 +28,8 @@ AS
   procedureName VARCHAR(100);
   jobID number(18,0);
   stepCt number(18,0);
+  
+  secNodeExists		int;
 
 BEGIN
 
@@ -87,13 +103,104 @@ BEGIN
     b.VALUETYPE_CD,
 	coalesce(f.tval_char,'EXP:PUBLIC')
     from I2B2METADATA.I2B2 b
-		,(select distinct modifier_cd, tval_char from observation_fact where concept_cd = 'SECURITY') f
-	where b.sourcesystem_cd = f.modifier_cd(+);
+		,(select distinct case when sourcesystem_cd like '%:%' then substr(sourcesystem_cd,1,instr(sourcesystem_cd,':')-1)
+							   else sourcesystem_cd end as sourcesystem_cd
+				,tval_char from observation_fact where concept_cd = 'SECURITY') f
+	where b.sourcesystem_cd = f.sourcesystem_cd(+);
     stepCt := stepCt + 1;
     cz_write_audit(jobId,databaseName,procedureName,'Insert security data into I2B2METADATA i2b2_secure',SQL%ROWCOUNT,stepCt,'Done');
 
     commit;
+	
+	--	check if SECURITY node exists in i2b2
+	
+	select count(*) into secNodeExists
+	from i2b2metadata.i2b2
+	where c_fullname = '\Public Studies\SECURITY\';
+	
+	if secNodeExists = 0 then
+		insert into i2b2metadata.i2b2
+		(c_hlevel
+		,c_fullname
+		,c_name
+		,c_synonym_cd
+		,c_visualattributes
+		,c_totalnum
+		,c_basecode
+		,c_metadataxml
+		,c_facttablecolumn
+		,c_tablename
+		,c_columnname
+		,c_columndatatype
+		,c_operator
+		,c_dimcode
+		,c_comment
+		,c_tooltip
+		,update_date
+		,download_date
+		,import_date
+		,sourcesystem_cd
+		,valuetype_cd
+		,i2b2_id
+		)
+		select 1 as c_hlevel
+			  ,'\Public Studies\SECURITY\' as c_fullname
+			  ,'SECURITY' as c_name
+			  ,'N' as c_synonym_cd
+			  ,'CA' as c_visualattributes
+			  ,null as c_totalnum
+			  ,null as c_basecode
+			  ,null as c_metadataxml
+			  ,'concept_cd' as c_facttablecolumn
+			  ,'concept_dimension' as c_tablename
+			  ,'concept_path' as c_columnname
+			  ,'T' as c_columndatatype
+			  ,'LIKE' as c_operator
+			  ,'\Public Studies\SECURITY\' as c_dimcode
+			  ,null as c_comment
+			  ,'\Public Studies\SECURITY\' as c_tooltip
+			  ,sysdate as update_date
+			  ,null as download_date
+			  ,sysdate as import_date
+			  ,null as sourcesystem_cd
+			  ,null as valuetype_cd
+			  ,I2B2_ID_SEQ.nextval as i2b2_id
+		from dual;
+			  
+		stepCt := stepCt + 1;
+		czx_write_audit(jobId,databaseName,procedureName,'Insert \Public Studies\SECURITY\ node to i2b2',SQL%ROWCOUNT,stepCt,'Done');
+		COMMIT;	
+	end if;
 
+	--	check if SECURITY node exists in concept_dimension
+	
+	select count(*) into secNodeExists
+	from i2b2demodata.concept_dimension
+	where concept_path = '\Public Studies\SECURITY\';
+	
+	if secNodeExists = 0 then
+		insert into i2b2demodata.concept_dimension
+		(concept_cd
+		,concept_path
+		,name_char
+		,update_date
+		,download_date
+		,import_date
+		,sourcesystem_cd
+		)
+		select 'SECURITY'
+			 ,'\Public Studies\SECURITY\'
+			 ,'SECURITY'
+			 ,sysdate
+			 ,sysdate
+			 ,sysdate
+			 ,null
+		from dual;
+		stepCt := stepCt + 1;
+		czx_write_audit(jobId,databaseName,procedureName,'Insert \Public Studies\SECURITY\ node to concept_dimension',SQL%ROWCOUNT,stepCt,'Done');
+		commit;
+	end if;
+	
     ---Cleanup OVERALL JOB if this proc is being run standalone
   IF newJobFlag = 1
   THEN
@@ -108,4 +215,4 @@ BEGIN
     cz_end_audit (jobID, 'FAIL');
 
 END;
-/ 
+/
