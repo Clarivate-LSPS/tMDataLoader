e6044e5077c318b6e1fbb83eba3f6b5a5e394502
Alex Lazarenko <Alex.Lazarenko@waveaccess.ru>
2016-05-24 12:47:44 +0300

    divide deleting into blocks by id and path

--- sql/postgres/procedures/I2B2_DELETE_ALL_DATA.sql
+++ sql/postgres/procedures/I2B2_DELETE_ALL_DATA.sql
@@ -39,6 +39,15 @@ BEGIN
   procedureName := 'i2b2_delete_all_data';
 
   select case when coalesce(currentjobid, -1) < 1 then cz_start_audit(procedureName, databaseName) else currentjobid end into jobId;
+  stepCt := 0;
+
+  if (path_string is null and trial_id is null) then
+    stepCt := stepCt + 1;
+    select cz_write_audit(jobId,databasename,procedurename, 'Path string and study id are null',1,stepCt,'ERROR') into rtnCd;
+    select cz_error_handler(jobid, procedurename, '-1', 'Application raised error') into rtnCd;
+    select cz_end_audit (jobId,'FAIL') into rtnCd;
+    return -16;
+  end if;
 
   if (path_string is null) then
     SELECT DISTINCT
@@ -46,14 +55,6 @@ BEGIN
     INTO pathString
     FROM i2b2demodata.concept_dimension
     WHERE sourcesystem_cd = trial_id;
-
-    if pathString is null then
-      stepCt := stepCt + 1;
-      select cz_write_audit(jobId,databasename,procedurename, 'No study with id ''' || COALESCE(trial_id, '<<null>>') || ''' found',1,stepCt,'ERROR') into rtnCd;
-      select cz_error_handler(jobid, procedurename, '-1', 'Application raised error') into rtnCd;
-      select cz_end_audit (jobId,'FAIL') into rtnCd;
-      return -16;
-    end if;
   else
     pathString := path_string;
     pathString := REGEXP_REPLACE('\' || pathString || '\','(\\){2,}', '\','g');
@@ -81,24 +82,22 @@ BEGIN
 	  TrialId := trial_id;
   end if;
 
-  select count(parent_concept_path) into topNodeCount
+
+  if (pathString is not null and (pathString != '' or pathString != '%')) then
+    select count(parent_concept_path) into topNodeCount
     from I2B2DEMODATA.concept_counts
     where
     concept_path = pathString;
 
-  if (topNodeCount > 0) then
-    select parent_concept_path into topNode
+    if (topNodeCount > 0) then
+      select parent_concept_path into topNode
       from I2B2DEMODATA.concept_counts
       where
       concept_path = pathString;
-  else
-    topNode := substring(pathString from 1 for position('\' in substring(pathString from 2))+1);
-  end if;
+    else
+      topNode := substring(pathString from 1 for position('\' in substring(pathString from 2))+1);
+    end if;
 
-
-  stepCt := 0;
-
-  if pathString != '' or pathString != '%' then
 	  stepCt := stepCt + 1;
     if (TrialID is null) then
       select cz_write_audit(jobId,databaseName,procedureName,'Starting I2B2_DELETE_ALL_DATA for ''' || pathString || '''',0,stepCt,'Done') into rtnCd;
@@ -106,25 +105,23 @@ BEGIN
       select cz_write_audit(jobId,databaseName,procedureName,'Starting I2B2_DELETE_ALL_DATA for ''' || pathString || ''' with id: ' || TrialId,0,stepCt,'Done') into rtnCd;
     end if;
 
-  --	delete all i2b2 nodes
+    --	delete all i2b2 nodes
+    select i2b2_delete_all_nodes(pathString,jobId) into rtnCd;
 
-  select i2b2_delete_all_nodes(pathString,jobId) into rtnCd;
+    --	delete any table_access data
+    delete from i2b2metadata.table_access
+    where c_fullname like pathString || '%' ESCAPE '`';
 
-  --	delete any table_access data
-  delete from i2b2metadata.table_access
-  where c_fullname like pathString || '%' ESCAPE '`';
-	--	delete any i2b2_tag data
-
-	delete from i2b2metadata.i2b2_tags
-	where path like pathString || '%' ESCAPE '`';
-	stepCt := stepCt + 1;
-	get diagnostics rowCt := ROW_COUNT;
-	select cz_write_audit(jobId,databaseName,procedureName,'Delete data for trial from I2B2METADATA i2b2_tags',rowCt,stepCt,'Done') into rtnCd;
-
+    --	delete any i2b2_tag data
+	  delete from i2b2metadata.i2b2_tags
+	  where path like pathString || '%' ESCAPE '`';
+	  stepCt := stepCt + 1;
+	  get diagnostics rowCt := ROW_COUNT;
+	  select cz_write_audit(jobId,databaseName,procedureName,'Delete data for trial from I2B2METADATA i2b2_tags',rowCt,stepCt,'Done') into rtnCd;
+  end if;
 
 	--	delete clinical data
-	if (trialId is not NUll)
-	then
+	if (trialId is not NUll) then
 		delete from gwas_plink.plink_data where study_id = trialId;
 		stepCt := stepCt + 1;
 		get diagnostics rowCt := ROW_COUNT;
@@ -300,10 +297,10 @@ BEGIN
 		stepCt := stepCt + 1;
 		get diagnostics rowCt := ROW_COUNT;
 		select cz_write_audit(jobId,databaseName,procedureName,'Delete trial from DEAPP de_subject_sample_mapping',rowCt,stepCt,'Done') into rtnCd;
-	end if;
 
-  delete from deapp.de_subject_snp_dataset
-	where trial_name = trialId;
+    delete from deapp.de_subject_snp_dataset
+    where trial_name = trialId;
+  end if;
 
 	stepCt := stepCt + 1;
   get diagnostics rowCt := ROW_COUNT;
@@ -316,14 +313,15 @@ BEGIN
   select cz_write_audit(jobId,databaseName,procedureName,'Delete data from DE_SUBJECT_ACGH_DATA',rowCt,stepCt,'Done') into rtnCd;
 
 	/*Check and delete top node, if removed node is last*/
-    stepCt := stepCt + 1;
-    select cz_write_audit(jobId,databaseName,procedureName,'Check and delete top node '||topNode||' if removed node is last',0,stepCt,'Done') into rtnCd;
-    select count(*) into countNodeUnderTop
-    from I2B2DEMODATA.concept_counts
-    where parent_concept_path = topNode;
+    if (topNode is not null) then
+      stepCt := stepCt + 1;
+      select cz_write_audit(jobId,databaseName,procedureName,'Check and delete top node '||topNode||' if removed node is last',0,stepCt,'Done') into rtnCd;
+      select count(*) into countNodeUnderTop
+      from I2B2DEMODATA.concept_counts
+      where parent_concept_path = topNode;
 
       stepCt := stepCt + 1;
-	  get diagnostics rowCt := ROW_COUNT;
+	    get diagnostics rowCt := ROW_COUNT;
       select cz_write_audit(jobId,databaseName,procedureName,'Check if need to remove top node '||topNode,rowCt,stepCt,'Done') into rtnCd;
 
       if (countNodeUnderTop = 0)
@@ -336,8 +334,7 @@ BEGIN
           select i2b2_delete_all_data(null, topNode, jobID) into rtnCd;
         end if;
       end if;
-
-  end if;
+    end if;
 
   perform cz_end_audit (jobID, 'SUCCESS') where coalesce(currentJobId, -1) <> jobId;
 
