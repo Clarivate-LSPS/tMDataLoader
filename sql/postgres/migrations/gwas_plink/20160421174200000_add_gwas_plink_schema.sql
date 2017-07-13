660a18aec3eeb08f95850731af52aee4f884327a
vadim <vadim.ustianskyi@thomsonreuters.com>
2017-07-12 19:08:35 -0700

    Fix for incorrect default values in plink_data table on PostgreSQL in tranSMART 16.2.

--- sql/postgres/migrations/gwas_plink/20160421174200000_add_gwas_plink_schema.sql
+++ sql/postgres/migrations/gwas_plink/20160421174200000_add_gwas_plink_schema.sql
@@ -4,6 +4,8 @@ BEGIN
 								FROM information_schema.schemata
 								WHERE schema_name = 'gwas_plink')
 	THEN
+		alter table gwas_plink.plink_data alter column study_id drop default;
+		alter table gwas_plink.plink_data alter column plink_data_id set default nextval('gwas_plink.seq_plink_data_id'::regclass);
 		RETURN;
 	END IF;
 
